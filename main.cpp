d3d77aa1613b558dbd4470a1dad2e28a35c0e814
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-16 18:55:14 +0530

    learning about events

--- main.cpp
+++ main.cpp
@@ -7,5 +7,18 @@
 
 
 int main(){
-  sf::Window window(sf::VideoMode(sf::Vector2u({500,500})), "Main Window");
+  sf::Window window(sf::VideoMode(sf::Vector2u({500, 500})), "Main Window");
+  while (window.isOpen()) {
+    while (std::optional event = window.pollEvent()) {
+      if (event->is < sf::Event::Closed>()) {
+	window.close();
+      } else if (const auto *key_pressed =
+                     event->getIf<sf::Event::KeyPressed>()) {
+        if (key_pressed->scancode == sf::Keyboard::Scancode::Escape) {
+	  window.close();
+        }
+      }
+    }
+  }
+
 }
