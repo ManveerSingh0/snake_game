550c62571c7db90aa2d4a871f4b14779aea15566
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-16 20:02:18 +0530

    learining about event visition

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 
 #include <SFML/Window.hpp>
+#include <SFML/Window/Event.hpp>
 #include <SFML/Window/Window.hpp>
 #include <iostream>
 
@@ -9,11 +10,11 @@
 int main(){
   sf::Window window(sf::VideoMode(sf::Vector2u({500, 500})), "Main Window");
   while (window.isOpen()) {
-    while (std::optional event = window.pollEvent()) {
+    while (std::optional<sf::Event> event = window.pollEvent()) {
       if (event->is < sf::Event::Closed>()) {
 	window.close();
-      } else if (const auto *key_pressed =
-                     event->getIf<sf::Event::KeyPressed>()) {
+      } else if (const sf::Event::KeyPressed *key_pressed =
+		 event->getIf<sf::Event::KeyPressed>()) {
         if (key_pressed->scancode == sf::Keyboard::Scancode::Escape) {
 	  window.close();
         }
