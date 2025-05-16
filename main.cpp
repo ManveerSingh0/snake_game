2f5a180c86a298e086d6eb0e62a072f022ffab66
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-16 20:59:24 +0530

    learning to draw sprite

--- main.cpp
+++ main.cpp
@@ -1,16 +1,24 @@
+//SFML specific libraries
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-
 #include <SFML/Window.hpp>
-#include <SFML/Window/Event.hpp>
-#include <SFML/Window/Window.hpp>
+
+//cpp std libraries
 #include <iostream>
 
 
 int main(){
-  sf::Window window(sf::VideoMode(sf::Vector2u({500, 500})), "Main Window");
+  sf::RenderWindow window(sf::VideoMode(sf::Vector2u({500, 500})), "Main Window");
   while (window.isOpen()) {
-    while (std::optional<sf::Event> event = window.pollEvent()) {
+    
+    //This is another way to close the window
+    // window.handleEvents(
+    // 			[&window](const sf::Event::Closed){
+    // 			  std::cout << "Event Closed\n";
+    // 			  window.close();
+    // 			}
+    //     );
+   while (std::optional<sf::Event> event = window.pollEvent()) {
       if (event->is < sf::Event::Closed>()) {
 	window.close();
       } else if (const sf::Event::KeyPressed *key_pressed =
@@ -18,8 +26,21 @@ int main(){
         if (key_pressed->scancode == sf::Keyboard::Scancode::Escape) {
 	  window.close();
         }
+      } else if (const sf::Event::Resized *resize =
+                     event->getIf<sf::Event::Resized>()) {
+	std::cout << "width: " << resize->size.x << '\n';
+	std::cout << "height: " << resize->size.y << '\n';
+      } else if (const sf::Event::FocusLost *focus =
+                     event->getIf<sf::Event::FocusLost>()) {
+	std::cout << "focust lost\n";
+      }else if (const sf::Event::FocusGained *focus =
+                     event->getIf<sf::Event::FocusGained>()) {
+	std::cout << "focust Gained\n";
       }
-    }
+   }
+
+   window.clear();
+   window.display();
   }
 
 }
