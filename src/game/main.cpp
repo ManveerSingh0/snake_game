5da8028ea70f4ecf4c1a116da816cf47c0459c97
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-21 22:43:21 +0530

    will make snake game tomorrow

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -2,8 +2,13 @@
 #include "raylib.h"
 
 
+struct Color red = {255,0,0,255};
+
+
 int main(int argc, char* argv[]) {
   InitWindow(200, 200, "Snake Game"); 
+  SetTargetFPS(60);
+
 
   while (!WindowShouldClose()) {
     BeginDrawing();
